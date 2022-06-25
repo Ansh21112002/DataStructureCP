7679b6f910957b43ccb3ec70562e72adc3ab6c21
The-Sanskar-Jain0901 <77044966+The-Sanskar-Jain0901@users.noreply.github.com>
2022-06-25 22:04:38 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,63 @@ using namespace std;
     *a = *b;
     *b = temp;
   }
+
+  void merge(int arr[], int l, int m, int r)
+{
+int i, j, k;
+int n1 = m - l + 1;
+int n2 = r - m;
+int L[n1], R[n2];
+for (i = 0; i < n1; i++)
+L[i] = arr[l + i];
+for (j = 0; j < n2; j++)
+R[j] = arr[m + 1+ j];
+
+i = 0;
+j = 0;
+k = l;
+while (i < n1 && j < n2)
+{
+if (L[i] <= R[j])
+{
+arr[k] = L[i];
+i++;
+}
+else
+{
+arr[k] = R[j];
+j++;
+}
+k++;
+}
+
+while (i < n1)
+{
+arr[k] = L[i];
+i++;
+k++;
+}
+
+while (j < n2)
+{
+arr[k] = R[j];
+j++;
+k++;
+}
+}
+  void mergeSort(int arr[], int l, int r)
+{
+if (l < r)
+{
+int m = l+(r-l)/2;
+mergeSort(arr, l, m);
+mergeSort(arr, m+1, r);
+merge(arr, l, m, r);
+}
+}
+
+
+
 int getMax(int array[], int n) {
   int max = array[0];
   for (int i = 1; i < n; i++)
@@ -137,15 +194,46 @@ void heapify(int arr[], int n, int i) {
       heapify(arr, i, 0);
     }
   }
+  int linearSearch(int arr[], int x)
+{
+    int i, n= sizeof(arr)/sizeof(arr[0]);
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+int binarySearch(int a[], int beg, int end, int val)    
+{    
+    int mid;    
+    if(end >= beg)     
+    {        mid = (beg + end)/2;    
+/* if the item to be searched is present at middle */  
+        if(a[mid] == val)    
+        {                 
+            return mid+1;    
+        }    
+            /* if the item to be searched is smaller than middle, then it can only be in left subarray */  
+        else if(a[mid] < val)     
+        {  
+            return binarySearch(a, mid+1, end, val);    
+        }    
+            /* if the item to be searched is greater than middle, then it can only be in right subarray */  
+        else     
+        {  
+            return binarySearch(a, beg, mid-1, val);    
+        }          
+    }    
+    return -1;     
+}
 int main ()
 {
     //set to the csv file location and reading input from csv file
-    ifstream csv_file("E:\\Study Material\\SY-Sem2\\Data Structures\\DS_CP\\random.csv");
+    ifstream csv_file("random.csv");
 
     if(!csv_file.is_open())
         std::cout << "File Not Open" << '\n';
 
-    // Empty vector holding all data from csv file
+
     vector<int> row;
     string row_data;
     int x=0;
@@ -160,11 +248,80 @@ int main ()
         
         int arr[row.size()];
         copy(row.begin(),row.end(), arr);
-        heapSort(arr,row.size());
-        quickSort(arr,0,row.size()-1);
+while(1){
+	        int ch;
 
-        for (int  i = 0; i < row.size(); i++)
+        	printf("Enter 1 for quick sort\nEnter 2 for heap sort \n3 for radix \n4 for merge\nEnter 5 for linearSearch\nEnter 6 for binarysearch\n");
+
+        
+        	scanf("%d",&ch);
+        	switch(ch){
+        		case 1:{
+        			quicksort(arr,0,row.size()-1);
+        			for (int  i = 0; i < row.size(); i++)
             cout << arr[i] << '\n';
-    
+            break;
+        			
+                 }
 
+					
+				
+				case 2:{
+					 heapSort(arr,row.size());
+					 for (int  i = 0; i < row.size(); i++)
+            cout << arr[i] << '\n';
+					break;
+				}
+				case 3:{
+					radixsort(arr, row.size());
+					for (int  i = 0; i < row.size(); i++)
+            cout << arr[i] << '\n';
+					break;
+				}
+				case 4:{
+					mergeSort(arr,0,row.size()-1);
+					for (int  i = 0; i < row.size(); i++)
+            cout << arr[i] << '\n';
+					break;
+				}
+
+				case 5:{
+					int e;
+					printf("Enter the number you want to search\n");
+					scanf("%d",&e);
+					int i, n=row.size();
+                    for (i = 0; i < n; i++){
+                    	if (arr[i] == e){
+                    	cout<<"The no is present at index "<<i<<'\n';
+                    	break;	
+						}
+                    	cout << i << '\n';
+                    	
+					}
+					if(i==n)printf("not found");
+					break;
+				}
+				case 6:{
+					quicksort(arr,0,row.size()-1);
+					int e;
+					printf("Enter the number you want to search\n");
+					scanf("%d",&e);
+					printf("The number is at index %d\n",binarySearch(arr,0,row.size()-1,e)); 
+					break;
+				}
+
+				default:
+					exit(0);
+			}
+		
+
+
+		ofstream myfile("file.xls");
+        int vsize = row.size();
+        for(int n=0; n<vsize; n++){
+            myfile << arr[n] << '\n';
+        }
+    }
 }
+
+    
